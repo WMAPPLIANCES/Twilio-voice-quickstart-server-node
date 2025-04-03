e62467b3c5641e18b6928943d0713ad04601e753
WMAPPLIANCES <148669330+WMAPPLIANCES@users.noreply.github.com>
2025-04-03 14:49:42 -0700

    Update twilio.js

--- twilio.js
+++ twilio.js
@@ -1,4 +1,3 @@
-// twilio.js corrigido e mais robusto
 const { VoiceResponse } = require('twilio').twiml;
 const client = require('twilio')(
   process.env.API_KEY,
@@ -10,10 +9,8 @@ exports.placeCall = async (req, res) => {
   const fromNumber = req.body.from;
   const toNumber = req.body.to;
 
-  console.log(`Iniciando chamada: ${fromNumber} -> ${toNumber}`);
-
   if (!fromNumber || !toNumber) {
-    console.error('Erro: Parâmetros from ou to não fornecidos.');
+    console.error('Parâmetros "from" ou "to" estão faltando.');
     return res.status(400).send('Parâmetros "from" e "to" são obrigatórios.');
   }
 
@@ -25,12 +22,11 @@ exports.placeCall = async (req, res) => {
       to: fromNumber,
       from: fromNumber,
     });
-
-    console.log('Chamada iniciada com sucesso, SID:', call.sid);
-    res.status(200).json({ sid: call.sid });
+    res.status(200).send({ sid: call.sid });
+    console.log(`Chamada iniciada com sucesso: ${call.sid}`);
   } catch (error) {
-    console.error('Erro na criação da chamada:', error.message);
-    res.status(500).send('Erro ao fazer chamada: ' + error.message);
+    console.error('Erro ao fazer a chamada:', error.message);
+    res.status(500).send(`Erro: ${error.message}`);
   }
 };
 
@@ -39,9 +35,8 @@ exports.connectClient = (req, res) => {
   const twiml = new VoiceResponse();
   const dial = twiml.dial();
   dial.number(toNumber);
-  
-  console.log(`Conectando ligação ao cliente: ${toNumber}`);
 
   res.type('text/xml');
   res.send(twiml.toString());
+  console.log(`Conectando cliente: ${toNumber}`);
 };
