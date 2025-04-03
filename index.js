78bbddf9d37a370de09dc82e604f86aefc14636d
WMAPPLIANCES <148669330+WMAPPLIANCES@users.noreply.github.com>
2025-04-03 14:44:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,21 @@
-// index.js
-
+// index.js atualizado
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const { VoiceResponse } = require('twilio').twiml;
-const twilio = require('twilio');
-require('dotenv').config();
+const { placeCall, connectClient } = require('./twilio');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Rota principal de teste
-app.get('/', (req, res) => res.send('Servidor ativo'));
+app.post('/place-call', placeCall);
+app.get('/connect-client', connectClient);
 
-// Rota para conectar com o cliente
-app.get('/connect-client', (req, res) => {
-  const to = req.query.to;
-  const twiml = new VoiceResponse();
-  const dial = twiml.dial();
-  dial.number(to);
-  res.type('text/xml');
-  res.send(twiml.toString());
-});
-
-// Rota para iniciar a chamada: liga para a empresa e conecta ao cliente
-app.post('/place-call', async (req, res) => {
-  const clientNumber = req.body.to || req.query.to;
-  const technicianNumber = req.body.from || process.env.CALLER_NUMBER;
-  const callbackUrl = req.protocol + '://' + req.get('host') + '/connect-client?to=' + encodeURIComponent(clientNumber);
-
-  const client = twilio(
-    process.env.API_KEY,
-    process.env.API_KEY_SECRET,
-    { accountSid: process.env.ACCOUNT_SID }
-  );
-
-  try {
-    const call = await client.calls.create({
-      url: callbackUrl,
-      to: technicianNumber,
-      from: technicianNumber
-    });
-    res.send(call.sid);
-  } catch (error) {
-    console.error('Erro ao fazer a chamada:', error);
-    res.status(500).send('Erro ao iniciar a chamada');
-  }
+app.get('/', (req, res) => {
+  res.send('Servidor Twilio funcionando.');
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`\u2705 Servidor rodando na porta ${port}`);
+  console.log(`🚀 Servidor rodando na porta ${port}`);
 });
-
