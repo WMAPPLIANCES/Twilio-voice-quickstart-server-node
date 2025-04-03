ae7f7d6c8235437dd2149196b47c5509fd66b8d9
WMAPPLIANCES <148669330+WMAPPLIANCES@users.noreply.github.com>
2025-04-03 14:16:48 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,4 +1,3 @@
-// server.js
 require('dotenv').config();
 
 const express = require('express');
@@ -8,9 +7,11 @@ const { tokenGenerator, welcome, incoming } = require('./src/server');
 const app = express();
 const http = require('http');
 
+// Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Rotas básicas
 app.get('/', (req, res) => res.send(welcome()));
 app.post('/', (req, res) => res.send(welcome()));
 
@@ -20,18 +21,19 @@ app.post('/accessToken', tokenGenerator);
 app.get('/incoming', (req, res) => res.send(incoming()));
 app.post('/incoming', (req, res) => res.send(incoming()));
 
-// Nova rota: conecta com cliente ao atender
-app.get('/connect-client', (req, res) => {
+// Rota para conectar com o cliente ao atender
+function connectClient(req, res) {
   const to = req.query.to;
   const twiml = new VoiceResponse();
   const dial = twiml.dial();
   dial.number(to);
   res.type('text/xml');
   res.send(twiml.toString());
-});
+}
+app.get('/connect-client', connectClient);
 
-// Nova rota: inicia chamada ligando para empresa
-app.post('/place-call', async (req, res) => {
+// Função separada para exportar corretamente
+async function placeCall(req, res) {
   const to = req.body.to || req.query.to;
   const fromNumber = process.env.CALLER_NUMBER;
   const callbackUrl = req.protocol + '://' + req.get('host') + '/connect-client?to=' + encodeURIComponent(to);
@@ -53,14 +55,17 @@ app.post('/place-call', async (req, res) => {
     console.error('Erro ao fazer a chamada:', error);
     res.status(500).send('Erro ao iniciar a chamada');
   }
-});
+}
+app.post('/place-call', placeCall);
 
+// Iniciar servidor
 const port = process.env.PORT || 3000;
 http.createServer(app).listen(port, () => {
   console.log('Servidor rodando na porta *:' + port);
 });
 
-
+// Exportações para testes ou reuso
 exports.placeCall = placeCall;
+exports.connectClient = connectClient;
 exports.incoming = incoming;
 exports.welcome = welcome;
